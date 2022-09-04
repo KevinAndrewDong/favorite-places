46cdc9a91b3f4ecf7794234662970b0296eb2e36
KevinAndrewDong <2970752254@qq.com>
2022-09-05 01:57:34 +0800

    feat: 拍照，展示模块

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,15 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+function PlaceItem({ place, onSelect }) {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{ uri: place.imageUri }} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default PlaceItem;
---
+++ models/place.js
@@ -0,0 +1,9 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location;
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
